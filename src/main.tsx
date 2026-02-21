e13337f5cd7b410fee36834f2b17b03f9e3b993a
olegdribling <63101414+olegdribling@users.noreply.github.com>
2026-02-21 17:36:23 +1100

    Refactor main.tsx to use React Router for navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
+import { LandingPage } from "./pages/LandingPage"
+import { LoginPage } from "./pages/LoginPage"
+import { RegisterPage } from "./pages/RegisterPage"
+import { AppPage } from "./pages/AppPage"
+import { RequireAuth } from "./auth/RequireAuth"
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/app/*"
+          element={
+            <RequireAuth>
+              <AppPage />
+            </RequireAuth>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
